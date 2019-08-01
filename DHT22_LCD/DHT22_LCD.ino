64f7973073f198592a1701526aa289920c208f0a
Jakub Machowski <machowski.jakub@o2.pl>
2019-08-01 10:50:40 +0200

    commit

--- DHT22_LCD/DHT22_LCD.ino
+++ DHT22_LCD/DHT22_LCD.ino
@@ -4,7 +4,7 @@
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  //Ustawienie adresu układu na 0x27.
 
-/* Ikona termometru */
+/* Ikona termometru*/
 byte tempIcon[] = {
   B00100,
   B01010,
@@ -39,33 +39,33 @@ byte stpIcon[] = {
   B00011,
   B00000
 };
- 
+
 dht DHT22;
 #define DHT22PIN 2 //Przypisanie pinu 2 Arduino jako odczyt z sensora
 #define L 13 //Przypisanie pinu 13 (diody LED wbudowanej w Arduino)
- 
-void setup() { 
-  lcd.begin(16,2); //Inicjalizacja LCD 16x2, lub 20x4.
-  lcd.createChar(1, tempIcon); //Konstruowanie ikony
-  lcd.createChar(2, stpIcon); //Konstruowanie ikony
-  lcd.createChar(3, wilIcon); //Konstruowanie ikony
+
+void setup() {
+  lcd.begin(16, 2); //Inicjalizacja LCD 16x2, lub 20x4.
+  lcd.createChar(1, stpIcon);
+  lcd.createChar(2, wilIcon);
+  lcd.createChar(3, tempIcon);
   pinMode(L, OUTPUT); //Ustawienie jako wyjście
 }
- 
+
 void loop() {
   digitalWrite(L, HIGH);
   int chk = DHT22.read(DHT22PIN); //Sprawdzenie stanu sensora, a następnie wyświetlenie komunikatu na monitorze szeregowym
-  lcd.setCursor(0,0);
-  lcd.write(byte(1));
-  lcd.setCursor(2,0);
-  lcd.print((float)DHT22.temperature, 1);
-  lcd.setCursor(6,0);
-  lcd.write(byte(2));
-  lcd.setCursor(9,0);
+  lcd.setCursor(0, 0);
   lcd.write(byte(3));
-  lcd.setCursor(11,0);
+  lcd.setCursor(2, 0);
+  lcd.print((float)DHT22.temperature, 1);
+  lcd.setCursor(6, 0);
+  lcd.write(byte(1));
+  lcd.setCursor(9, 0);
+  lcd.write(byte(2));
+  lcd.setCursor(11, 0);
   lcd.print((float)DHT22.humidity, 1);
-  lcd.setCursor(15,0);
+  lcd.setCursor(15, 0);
   lcd.print("%");
   digitalWrite(L, LOW);
   delay(1000);
